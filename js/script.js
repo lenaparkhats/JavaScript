c971980ee2e698353011bf23ac0438bf9a339ec9
Lena <lenaparkhats@gmail.com>
2020-03-02 23:21:17 +0300

    commit1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const addCar = document.getElementById('addCar');
+    const poiskCar = document.getElementById('poiskCar');
+    // console.log(addCar, poiskCar);
+    addCar.addEventListener('click', () => {
+
+    })
+})
